c134bcf28a1304d743e5e3799b24e6d38422a64c
Catherine Salazar <catcreates13@gmail.com>
2018-09-26 01:41:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,14 +8,14 @@ class Application
 
    if req.path.match(/items/)
      item_name = req.path.split("/items").last
-     item = @@items.find{|i| i.name == item_name}
+     if item = @@items.find{|i| i.name == item_name}
 
      resp.write item.price
 
-   elsif
+   else
      resp.write "Route not found"
      resp.status = 404
-
+   end
 
    else
      resp.write "Item not found"
