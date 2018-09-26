bcfe0caa584536a9059233eec1f3200fe3536842
Catherine Salazar <catcreates13@gmail.com>
2018-09-26 01:17:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,22 @@ class Application
   def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
-end
+
+  if req.path=="/item"
+     resp.write "You requested the songs"
+     
+   elsif 
+     resp.write "Route not found"
+     resp.status = 404
+
+   else
+     resp.write "Item not found"
+     resp.status = 400
+   end
+
+   
+
+   resp.finish
+ end
 
 end
