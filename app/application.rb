ff11cc0f152691da7a49691376f9cae5dbf692c6
Catherine Salazar <catcreates13@gmail.com>
2018-09-26 01:34:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,15 @@
 class Application
 
+  @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
+
   def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
 
    if req.path.match(/items/)
      item_name = req.path.split("/items").last
-  
+     item = @@items.find{|i| i.name == item_name}
+
 
    elsif
      req.path == "/items"
